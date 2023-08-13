1809721f71c4d5795a7947f277d6a2487964f1e8
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:00:05 +0300

    Create ecu_seven_segment_cfg.h

---
+++ ECU_Layer/7_Segment/ecu_seven_segment_cfg.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   ecu_seven_segment_cfg.h
+ * Author: Salah Bendary
+ *
+ * Created on 04 August 2023, 17:43
+ */
+
+
+#ifndef ECU_SEVEN_SEGMENT_CFG_H
+#define	ECU_SEVEN_SEGMENT_CFG_H
+
+/* Section : Includes */
+
+/* Section: Macro Declarations */
+
+/* Section: Macro Functions Declarations */
+
+/* Section: Data Type Declarations */
+
+/* Section: Function Declarations */
+
+#endif	/* ECU_SEVEN_SEGMENT_CFG_H */
