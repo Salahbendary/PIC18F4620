778e3f0c2508d6268d3af9a3d0e34e385a6cf38f
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:00:51 +0300

    Create ecu_dc_motor_cfg.h

---
+++ ECU_Layer/DC_Motor/ecu_dc_motor_cfg.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ecu_dc_motor_cfg.h
+ * Author: Salah Bendary
+ *
+ * Created on 04 August 2023, 17:31
+ */
+
+#ifndef ECU_DC_MOTOR_CFG_H
+#define	ECU_DC_MOTOR_CFG_H
+
+
+/* ----------------- Includes -----------------*/
+
+/* ----------------- Macro Declarations -----------------*/
+
+/* ----------------- Macro Functions Declarations -----------------*/
+
+/* ----------------- Data Type Declarations -----------------*/
+
+/* ----------------- Software Interfaces Declarations -----------------*/
+
+#endif	/* ECU_DC_MOTOR_CFG_H */
+
