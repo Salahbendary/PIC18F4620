b1eedd093234c02b794b23e10e21d77c7df499b1
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:01:41 +0300

    Create device_config.h

---
+++ MCAL_Layer/device_config.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   device_config.h
+ * Author: Salah Bendary
+ *
+ * Created on 19 July 2023, 18:30
+ */
+
+#ifndef DEVICE_CONFIG_H
+#define	DEVICE_CONFIG_H
+
+#define _XTAL_FREQ 4000000UL
+
+#endif	/* DEVICE_CONFIG_H */
+
