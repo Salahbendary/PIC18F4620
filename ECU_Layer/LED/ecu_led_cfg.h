49fffbfe1a63a77045b520c9f5ae136f4cc67a23
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:01:48 +0300

    Create ecu_led_cfg.h

---
+++ ECU_Layer/LED/ecu_led_cfg.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   ecu_led_cfg.h
+ * Author: Salah Bendary
+ *
+ * Created on 24 July 2023, 18:04
+ */
+
+#ifndef ECU_LED_CFG_H
+#define	ECU_LED_CFG_H
+
+/* Section : Includes */
+
+
+/* Section : Macro Functions Declarations */
+
+
+/* Section : Data Type Declaration */
+
+
+/* Section : Macro Declaration */
+
+
+/* Section : Function Declaration */
+#endif	/* ECU_LED_CFG_H */
+
