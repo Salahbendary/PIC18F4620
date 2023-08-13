98fdb714012ac8e2ca6f5635a3f629587b20d7f9
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:02:17 +0300

    Create ecu_keypad_cfg.h

---
+++ ECU_Layer/Keypad/ecu_keypad_cfg.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   ecu_keypad_cfg.h
+ * Author: Salah Bendary
+ * https://www.linkedin.com/in/salahbendary/
+ * Created on August 5, 2023, 5:03 PM
+ */
+
+#ifndef ECU_KEYPAD_CFG_H
+#define	ECU_KEYPAD_CFG_H
+
+/* ----------------- Includes -----------------*/
+
+/* ----------------- Macro Declarations -----------------*/
+
+/* ----------------- Macro Functions Declarations -----------------*/
+
+/* ----------------- Data Type Declarations -----------------*/
+
+/* ----------------- Software Interfaces Declarations -----------------*/
+
+#endif	/* ECU_KEYPAD_CFG_H */
+
