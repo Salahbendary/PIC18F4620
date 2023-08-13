997bbeb48c4c789b12a26d97df20c8e8f11d803c
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:01:55 +0300

    Create std_libraries.h

---
+++ MCAL_Layer/std_libraries.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   std_libraries.h
+ * Author: Salah Bendary
+ *
+ * Created on 19 July 2023, 18:17
+ */
+
+#ifndef STD_LIBRARIES_H
+#define	STD_LIBRARIES_H
+#include "../compiler.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif	/* STD_LIBRARIES_H */
+
