1051535019fe78fcfd62156c84159eb9aa7fd0c2
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:01:00 +0300

    Create ecu_layer_init.c

---
+++ ECU_Layer/ecu_layer_init.c
@@ -0,0 +1,13 @@
+/* 
+ * File:   ecu_layer_init.c
+ * Author: Salah Bendary
+ * https://github.com/Salahbendary/
+ * Created on 31 July 2023, 22:30
+ */
+
+#include "ecu_layer_init.h"
+
+void ecu_layer_intialize(void){
+    Std_ReturnType ret = E_NOT_OK;
+    
+}
