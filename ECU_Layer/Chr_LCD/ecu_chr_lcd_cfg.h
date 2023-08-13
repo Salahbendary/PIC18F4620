c8d66c3cb08046b1853743e4f08d2d0113aa8d04
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:00:41 +0300

    Create ecu_chr_lcd_cfg.h

---
+++ ECU_Layer/Chr_LCD/ecu_chr_lcd_cfg.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ecu_chr_lcd_cfg.h
+ * Author: Salah Bendary
+ * https://www.linkedin.com/in/salahbendary/
+ * Created on August 7, 2023, 5:46 PM
+ */
+
+
+#ifndef ECU_CHR_LCD_CFG_H
+#define	ECU_CHR_LCD_CFG_H
+
+/* ----------------- Includes -----------------*/
+
+/* ----------------- Macro Declarations -----------------*/
+
+/* ----------------- Macro Functions Declarations -----------------*/
+
+/* ----------------- Data Type Declarations -----------------*/
+
+/* ----------------- Software Interfaces Declarations -----------------*/
+
+#endif	/* ECU_CHR_LCD_CFG_H */
+
