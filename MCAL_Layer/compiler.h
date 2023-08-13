8c49acf5a9f4513cf61b0e849006ad63a02ae0d7
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:01:39 +0300

    Create compiler.h

---
+++ MCAL_Layer/compiler.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   compiler.h
+ * Author: Salah Bendary
+ *
+ * Created on 19 July 2023, 18:18
+ */
+
+#ifndef COMPILER_H
+#define	COMPILER_H
+#include <xc.h>
+#endif	/* COMPILER_H */
+
+ 
