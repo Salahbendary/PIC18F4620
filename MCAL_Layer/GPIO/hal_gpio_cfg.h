4e8d2cc8db182582a359661cf01fcf0b377327f1
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:02:01 +0300

    Create hal_gpio_cfg.h

---
+++ MCAL_Layer/GPIO/hal_gpio_cfg.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   hal_gpio_cfg.h
+ * Author: Salah Bendary
+ *
+ * Created on 29 July 2023, 20:11
+ */
+
+#ifndef HAL_GPIO_CFG_H
+#define	HAL_GPIO_CFG_H
+
+/* Section : Includes */
+
+/* Section : Macro Functions Declarations */
+
+/* Section : Data Type Declaration */
+
+/* Section : Macro Declaration */
+
+#define CONGIF_ENABLE   0x01
+#define CONFIG_DISABLE  0x00
+
+/* Section : Function Declaration */
+
+#endif	/* HAL_GPIO_CFG_H */
+
