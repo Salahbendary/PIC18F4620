bd4c37df0577d275eca451f86a620ec81bbcfa4a
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:01:25 +0300

    Create ecu_button_cfg.h

---
+++ ECU_Layer/PUSH_BUTTON/ecu_button_cfg.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   ecu_button_cfg.h
+ * Author: Salah Bendary
+ *
+ * Created on 31 July 2023, 20:31
+ */
+
+#ifndef ECU_BUTTON_CFG_H
+#define	ECU_BUTTON_CFG_H
+
+/* Section : Includes */
+
+/* Section : Macro Functions Declarations */
+
+/* Section : Data Type Declaration */
+
+/* Section : Macro Declaration */
+
+/* Section : Function Declaration */
+
+#endif	/* ECU_BUTTON_CFG_H */
+
