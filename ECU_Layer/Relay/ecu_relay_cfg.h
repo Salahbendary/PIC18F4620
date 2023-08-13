8f0a241c7ec7b3a6cb4eef9337c12d511591def8
Salah Bendary <salahbendary1@gmail.com>
2023-08-13 18:01:34 +0300

    Create ecu_relay_cfg.h

---
+++ ECU_Layer/Relay/ecu_relay_cfg.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   ecu_relay_cfg.h
+ * Author: Salah Bendary
+ *
+ * Created on 04 August 2023, 17:08
+ */
+
+#ifndef ECU_RELAY_CFG_H
+#define	ECU_RELAY_CFG_H
+
+/* ----------------- Includes -----------------*/
+
+/* ----------------- Macro Declarations -----------------*/
+
+/* ----------------- Macro Functions Declarations -----------------*/
+
+/* ----------------- Data Type Declarations -----------------*/
+
+/* ----------------- Software Interfaces Declarations -----------------*/
+
+#endif	/* ECU_RELAY_CFG_H */
