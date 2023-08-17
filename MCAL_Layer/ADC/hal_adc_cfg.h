9710c9c5c1375c63ec4b10745e2ef07ce6105f11
Salah Bendary <salahbendary1@gmail.com>
2023-08-17 12:11:46 +0300

    Create hal_adc_cfg.h

---
+++ MCAL_Layer/ADC/hal_adc_cfg.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   hal_adc_cfg.h
+ * Author: Salah Bendary
+ 
+ * Created on August 17, 2023, 7:24 PM
+ */
+
+#ifndef HAL_ADC_CFG_H
+#define	HAL_ADC_CFG_H
+
+/* ----------------- Includes -----------------*/
+
+/* ----------------- Macro Declarations -----------------*/
+
+/* ----------------- Macro Functions Declarations -----------------*/
+
+/* ----------------- Data Type Declarations -----------------*/
+
+/* ----------------- Software Interfaces Declarations -----------------*/
+
+#endif	/* HAL_ADC_CFG_H */
+
