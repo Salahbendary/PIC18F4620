eff7fdbaf4b1ec65eaab8c170241a8847cad582a
Salah Bendary <salahbendary1@gmail.com>
2023-08-29 18:26:48 +0300

    Create hal_usart_cfg.h

---
+++ MCAL_Layer/usart/hal_usart_cfg.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   hal_usart_cfg.h
+ * Author: Salah Bendary
+ 
+ * Created on August 27, 2023, 18:42 PM
+ */
+
+#ifndef HAL_USART_CFG_H
+#define	HAL_USART_CFG_H
+
+/* ----------------- Includes -----------------*/
+
+/* ----------------- Macro Declarations -----------------*/
+
+/* ----------------- Macro Functions Declarations -----------------*/
+
+/* ----------------- Data Type Declarations -----------------*/
+
+/* ----------------- Software Interfaces Declarations -----------------*/
+
+#endif	/* HAL_USART_CFG_H */
+
